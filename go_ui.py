39434ac7ad14c71b062796105b06e2259602a975
EricFalkenberg <exf4789@rit.edu>
2017-01-08 21:56:14 -0500

    Added means to provide user input

--- go_ui.py
+++ go_ui.py
@@ -8,6 +8,16 @@ def main():
     curses.cbreak()
     standard_screen.keypad(1)
 
+    ## TEST
+    standard_screen.addstr(1, 5, "Lorem Ipsum Dolor sit amet", curses.A_BOLD)
+    standard_screen.refresh()
+    while True:
+        user_input = standard_screen.getch()
+        if user_input == ord('q'):
+            break
+        else:
+            continue
+
     ## TEARDOWN
     curses.echo()
     curses.nocbreak()
