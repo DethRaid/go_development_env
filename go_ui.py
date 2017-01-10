84dea0905a793de64fe158bea26eb1bd933d7f26
EricFalkenberg <exf4789@rit.edu>
2017-01-09 21:12:19 -0500

    game area is now drawn based on overall screen dimensions

--- go_ui.py
+++ go_ui.py
@@ -7,9 +7,10 @@ def main():
     curses.cbreak()
     standard_screen.keypad(1)
     standard_screen.refresh()
+    WINDOW_HEIGHT, WINDOW_LENGTH = standard_screen.getmaxyx()
 
     ## TEST
-    game_area = curses.newwin(30, 80, 0, 0)
+    game_area = curses.newwin(WINDOW_HEIGHT-1, WINDOW_LENGTH//2, 0, 0)
     game_area.box()
     game_area.addstr(1, 1, "Match: A vs. B", curses.A_BOLD)
     game_area.refresh()
