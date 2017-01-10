cf94265d3e2113c3caf467f5758cd050938301d8
EricFalkenberg <exf4789@rit.edu>
2017-01-09 21:01:57 -0500

    box -> game_area

--- go_ui.py
+++ go_ui.py
@@ -1,19 +1,18 @@
 import curses
 
-
 def main():
     ## SETUP
     standard_screen = curses.initscr()
     curses.noecho()
     curses.cbreak()
     standard_screen.keypad(1)
+    standard_screen.refresh()
 
     ## TEST
-    box = curses.newwin(20, 30, 2, 5)
-    box.box()
-    standard_screen.addstr(1, 5, "Lorem Ipsum Dolor sit amet", curses.A_BOLD)
-    standard_screen.refresh()
-    box.refresh()
+    game_area = curses.newwin(30, 80, 0, 0)
+    game_area.box()
+    game_area.addstr(1, 1, "Match: A vs. B", curses.A_BOLD)
+    game_area.refresh()
     while True:
         user_input = standard_screen.getch()
         if user_input == ord('q'):
